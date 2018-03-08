9d1d84ba1300ad0689b01efe5f82101b2fb2d9e2
WilliamCui <33232366+TaillessSheep@users.noreply.github.com>
2018-03-08 06:11:58 -0500

    yo

--- sketch_mar07a/sketch_mar07a.ino
+++ sketch_mar07a/sketch_mar07a.ino
@@ -1,6 +1,6 @@
 /*
 Hi everyone!
-My name is Hugo, I hope you guys are having a nice day!
+My name is Hu-go, I hope you guys are having a nice day!
 I'll explain what the code does at every step so that you may control the car from your phone!
 */
 
@@ -11,6 +11,9 @@ SoftwareSerial BT(0,1);
 
 Servo turn;   //This is the name of our servo (the blue machine)
 Servo power; //C is for continuous servo (the back wheels).
+
+void checkCommand();
+void modeChange();
 
 int UltraTrig=3; //These are the pins which will connect to the ultrasonic sensor.
 int UltraEcho=2; //The number corresponds to the pin number on the arduino board.
@@ -26,9 +29,6 @@ int angle=40;
 //40 is left
 
 float UltraSens; //This will store the value given by the sensor, it can hold 15 decimal places (0 for integers)
-
-int playMode = 0; // 0: stop mode; 1: drive mode; 2: smart stop; 3: roomba mode;
-
 
 int playMode = 0; // 0: stop mode; 1: smart stop; 2: roomba mode;
 char command='F';// Command received from your phone
@@ -54,7 +54,7 @@ void setup() {
     //The delay of 100ms is to prevent the arduino from looping too often while waiting.
   }
   
-  serv.write(70); //Tells the car to begin as going straight ahead.
+  turn.write(70); //Tells the car to begin as going straight ahead.
 }
 
 void loop() {
@@ -66,25 +66,23 @@ void loop() {
       }
     else if (commandType == 1 and playMode == 1){// in manual drive(playMode 1
       switch(command){
-        case 'A': power.writeMilliseconds(2000);
+        case 'A': power.writeMicroseconds(2000);
           break;
-        case 'B': power.writeMilliseconds(1000);
+        case 'B': power.writeMicroseconds(1000);
           break;
         case 'C': turn.write(40);
           break;
         case 'D': turn.write(100);
           break;
       }
+     }
     else{
       // it is a useless command
       // or not in manual drive mode (playMode 2)
       // so we dont do anything
       }
-<<<<<<< HEAD
-=======
     delay(200);
       
->>>>>>> d5ec86c681a9bc90654cf55fcac857e86d81f1da
     }
   if(playMode == 0){};
 }
