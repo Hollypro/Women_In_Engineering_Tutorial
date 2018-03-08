35c763bcaf85030b37dccf328c228b7e534cfbd0
Hugo Provencher <hugo1507@msn.com>
2018-03-08 02:47:00 -0500

    Update sketch_mar07a.ino

--- sketch_mar07a/sketch_mar07a.ino
+++ sketch_mar07a/sketch_mar07a.ino
@@ -4,8 +4,8 @@ My name is Hugo, I hope you guys are having a nice day!
 I'll explain what the code does at every step so that you may control the car from your phone!
 */
 
-#include <servo.h> //This tells the program to import some extra functions not usually needed.
-#include <BTSerial.h>
+#include <Servo.h> //This tells the program to import some extra functions not usually needed.
+// #include <BTSerial.h>
 
 Servo serv;   //This is the name of our servo (the blue machine)
 
@@ -15,21 +15,39 @@ int UltraEcho=11; //The number corresponds to the pin number on the arduino boar
 int MotorControlPin1=10;  //These are the pins for the motor controller (the black block)
 int MotorControlPin2=9;
 
-float UltraSens;          //This will store the value given by the sensor, it can hold 15 decimal places (0 for integers)
+int ServPin=3;
+int angle=40;
+//The angle determines the direction of the car.
+//70 is straight ahead
+//100 is right
+//40 is left
+
+float UltraSens; //This will store the value given by the sensor, it can hold 15 decimal places (0 for integers)
 
 
 void setup() {
-  pinMode(UltraTrig,OUTPUT);
-  pinMode(UltraEcho,OUTPUT);
+  serv.attach(ServPin);
+  //Tells the arduino that our servo is controlled by ServPin
+ 
+  pinMode(UltraTrig,OUTPUT); //This sets our pins as either input (collected data)
+  pinMode(UltraEcho,INPUT);  //Or output (transmitted data)
  
   pinMode(MotorControlPin1,OUTPUT);
   pinMode(MotorControlPin2,OUTPUT);
 
-  Serial.begin(9600);
-  Serial.begin(9600);
+  Serial.begin(9600); //This is for communication between different devices
+  //In this case, it is between the blutooth module and the arduino.
+  
+  while(!Serial){
+    delay(100);
+    //This makes the arduino wait until serial begins to work.
+    //The delay of 100ms is to prevent the arduino from looping too often while waiting.
+  }
+  
+  serv.write(70); //Tells the car to begin as going straight ahead.
 }
 
 void loop() {
-  Serial.
- 
+  analogWrite(MotorControlPin1,120);
+  analogWrite(MotorControlPin2,0); //tells the motor to go forward at half speed (it's too fast otherwise) 
 }
