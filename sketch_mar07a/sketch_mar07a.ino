266a5ebad2fee0ec081838a3ce64b43992c11a80
WilliamCui <33232366+TaillessSheep@users.noreply.github.com>
2018-03-08 06:23:38 -0500

    wall

--- sketch_mar07a/sketch_mar07a.ino
+++ sketch_mar07a/sketch_mar07a.ino
@@ -132,6 +132,7 @@ void roombaMode(){
   }
 //**********************************************************
 //
-void wall(){
-  
+bool wall(){
+  return 1;// when a object detected
+  return 0;// when it is safe
   }
